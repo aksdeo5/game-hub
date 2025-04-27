89eab418ffe3f795e5d055168364c5502bf3a753
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-04-27 22:26:31 +0530

    Displaying Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+	score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+	const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+	return (
+		<Badge
+			width="fit-content"
+			paddingX={2}
+			fontSize="14px"
+			colorPalette={color}
+		>
+			{score}
+		</Badge>
+	);
+};
+
+export default CriticScore;
