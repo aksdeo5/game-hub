7d866d6b2b94bd70725b124117450f139f456c98
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-04-30 18:22:41 +0530

    Handling Games without an Image

--- src/services/imageUrl.ts
+++ src/services/imageUrl.ts
@@ -1,4 +1,8 @@
+import noImage from "@/assets/no-image-placeholder.webp";
+
 const getCorppedImageUrl = (url: string) => {
+	if (!url) return noImage;
+
 	const target = "media/";
 
 	const index = url.indexOf(target) + target.length;
