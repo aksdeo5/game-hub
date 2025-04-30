b2774b5c6dc81b25fa9c787ddd2cd5e914556708
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-04-30 20:46:07 +0530

    Improving the GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,14 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCorppedImageUrl from "@/services/imageUrl";
-import { Button, HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
+import {
+	Button,
+	Heading,
+	HStack,
+	Image,
+	List,
+	Spinner,
+	Text,
+} from "@chakra-ui/react";
 
 interface Props {
 	onSelectGenre: (genre: Genre) => void;
@@ -15,27 +23,36 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 	if (isLoading) return <Spinner />;
 
 	return (
-		<List.Root variant="plain">
-			{data.map((genre) => (
-				<List.Item key={genre.id} paddingY="5px">
-					<Button
-						variant={genre.id === selectedGenre?.id ? "solid" : "ghost"}
-						width="100%"
-						justifyContent="left"
-						onClick={() => onSelectGenre(genre)}
-					>
-						<HStack>
-							<Image
-								boxSize="32px"
-								borderRadius={8}
-								src={getCorppedImageUrl(genre.image_background)}
-							/>
-							<Text>{genre.name}</Text>
-						</HStack>
-					</Button>
-				</List.Item>
-			))}
-		</List.Root>
+		<>
+			<Heading as="h2" marginBottom={3}>
+				Genres
+			</Heading>
+			<List.Root variant="plain">
+				{data.map((genre) => (
+					<List.Item key={genre.id} paddingY="5px">
+						<Button
+							variant={genre.id === selectedGenre?.id ? "solid" : "ghost"}
+							width="100%"
+							paddingX="unset"
+							justifyContent="left"
+							onClick={() => onSelectGenre(genre)}
+						>
+							<HStack>
+								<Image
+									boxSize="32px"
+									borderRadius={8}
+									objectFit="cover"
+									src={getCorppedImageUrl(genre.image_background)}
+								/>
+								<Text whiteSpace="normal" textAlign="left">
+									{genre.name}
+								</Text>
+							</HStack>
+						</Button>
+					</List.Item>
+				))}
+			</List.Root>
+		</>
 	);
 };
 
