592c94b274210af20e542e1b7f3da7bf3fd49953
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-04-30 22:22:08 +0530

    Shipping Static Data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,5 @@
-import useData from "./useData";
+import platforms from "@/data/platforms";
 
-interface Platform {
-	id: number;
-	name: string;
-	slug: string;
-}
-
-const useGames = () => useData<Platform>("/platforms/lists/parents");
+const useGames = () => ({ data: platforms, isLoading: false, error: null });
 
 export default useGames;
