22747456dbec3db08d78a7b426eacbd142fdeba7
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-04-30 20:52:08 +0530

    Improving GameCard and GameGrid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,13 +15,13 @@ const GameCard = ({ game }: Props) => {
 			<Card.Root>
 				<Image src={getCorppedImageUrl(game.background_image)} alt="" />
 				<Card.Body gap="2">
-					<Card.Title>{game.name}</Card.Title>
-					<HStack justifyContent="space-between">
+					<HStack justifyContent="space-between" marginBlockStart={3}>
 						<PlatformIconsList
 							platforms={game.parent_platforms.map((p) => p.platform)}
 						/>
 						<CriticScore score={game.metacritic} />
 					</HStack>
+					<Card.Title>{game.name}</Card.Title>
 				</Card.Body>
 			</Card.Root>
 		</GameCardContainer>
